b4dfee5a6882cf136b4176e9f18f5458d736f993
Hannah Carnes <hecarnes@gmail.com>
2018-04-08 18:28:45 -0400

    Remove default styling and replace with material ui

--- greenthumb-client/src/App.js
+++ greenthumb-client/src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Button from 'material-ui/Button';
+import LocalFlorist from 'material-ui-icons/LocalFlorist';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header>
+          <h1>Welcome to Greenthumb Gardens</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p>
+          <Button><LocalFlorist />Hello world</Button>
         </p>
       </div>
     );

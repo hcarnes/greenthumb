20940f54fb2a2563aa1517bb9835828346c6eb2e
Hannah Carnes <hecarnes@gmail.com>
2018-04-08 18:53:45 -0400

    Basic grid layout

--- greenthumb-client/src/App.js
+++ greenthumb-client/src/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
-import Button from 'material-ui/Button';
-import LocalFlorist from 'material-ui-icons/LocalFlorist';
+import { withStyles } from 'material-ui/styles';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
 import './App.css';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  contentPaper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'left',
+    color: theme.palette.text.secondary,
+  },
+  primaryPaper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.primary,
+  },
+});
 
 class App extends Component {
   render() {
+    const { classes } = this.props
     return (
-      <div>
-        <header>
-          <h1>Welcome to Greenthumb Gardens</h1>
-        </header>
-        <p>
-          <Button><LocalFlorist />Hello world</Button>
-        </p>
+      <div className={classes.root}>
+        <Grid container spacing={8}>
+          <Grid item xs={12}>
+            <Paper className={classes.primaryPaper}>Welcome to Greenthumb Gardens</Paper>
+          </Grid>
+          <Grid item xs={12} sm={10}>
+            <Paper className={classes.contentPaper}>Map</Paper>
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <Paper className={classes.contentPaper}>List</Paper>
+          </Grid>
+        </Grid>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);

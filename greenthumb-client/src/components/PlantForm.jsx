00d87708e8c267c12d2f5c256ccb3ef82f2c9047
Hannah Carnes <hecarnes@gmail.com>
2018-04-12 12:56:29 -0400

    create PlantForm component

---
+++ greenthumb-client/src/components/PlantForm.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class PlantForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+    };
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_PLANT',
+      plant: this.state.text
+    });
+    this.setState({
+      text: '',
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input
+            type="text"
+            value="this.state.text"
+            onChange={(event) => this.handleOnChange(event)} />
+          <input type="submit" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default PlantInput;

29cf2e712562c553f4994733265e23e77f72338a
Hannah Carnes <hecarnes@gmail.com>
2018-04-12 14:11:37 -0400

    minor update to READ.ME

--- greenthumb-client/src/actions/addPlant.js
+++ greenthumb-client/src/actions/addPlant.js
@@ -1,6 +1,16 @@
-export const addPlant = (plant) => {
-  return {
-    type: 'ADD_PLANT',
-    plant
+// export const addPlant = (plant) => {
+//   return {
+//     type: 'ADD_PLANT',
+//     plant
+//   };
+// };
+
+export function addPlant(plant) {
+  return async (dispatch) => {
+    dispatch({ type: 'START_ADDING_PLANT' });
+
+    const plantsResponse = await fetch('http://localhost:3001/plants')
+    const gardens = await gardensResponse.json()
+    dispatch({ type: 'ADD_GARDENS', gardens })
   };
-};
+}
